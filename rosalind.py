44716dff5c75cdcce44246f8fe16f548e4365fe2
Virginie Jouffret <virginie.jouffret@univ-tlse3.fr>
2024-12-27 17:35:47 +0100

    clean

--- rosalind.py
+++ rosalind.py
@@ -1,23 +1,23 @@
-#################################################################
-###################     Rosalind     ############################
-#################################################################
+# ################################################################
+# ##################     Rosalind     ############################
+# ################################################################
 
 
-###################     FONCTIONS    ############################
+# ##################     FONCTIONS    ############################
 
 
-def read_fichier(fichier) :
-	F=open(fichier,'r')
-	fic=F.read()
-	F.close()
-	return [i for i in fic.split('\n') if i]
+def read_fichier(fichier):
+	with open(fichier, 'r') as f_input:
+		return [i for i in f_input.read().split('\n') if i]
 
 
-###################     Exercices    ############################
+# ##################     Exercices    ############################
 
 
-#### INI 1
+# ##### INI 1
+
 import this
+
 """
 The Zen of Python, by Tim Peters
 
@@ -43,86 +43,100 @@ Namespaces are one honking great idea -- let's do more of those!
 """
 
 
-#### Variables and somes arithmetic
-a=input("a = ")
-b=input("b = ")
+# ##### Variables and somes arithmetic
+a = input("a = ")
+b = input("b = ")
 reponse = a*a+b*b
 print(reponse)
 
 
-##### INI5
-F=open('rosalind_ini5.txt','r')
-fic=F.readlines()
+# ##### INI5
+F = open('rosalind_ini5.txt', 'r')
+fic = F.readlines()
 F.close()
 
-F=open('resultats.txt','w')
-for i in range(1,len(fic)) :
-	if i%2 :
+F = open('resultats.txt', 'w')
+for i in range(1, len(fic)):
+	if i % 2:
 		F.write(fic[i])
 
 F.close()
 
 
-###### INI6
-F=open('rosalind_ini6.txt','r')
-fic=F.read()
+# ##### INI6
+F = open('rosalind_ini6.txt', 'r')
+fic = F.read()
 F.close()
 
-dico={}
-fic=fic.split('\r\n')
-for i in range(len(fic)) :
-	fic[i]=fic[i].split(' ')
-	for v in fic[i] :
-		if not dico.has_key(v) :
-			dico[v]=1
-		else :
-			dico[v]+=1
+dico = {}
+fic = fic.split('\r\n')
+for i in range(len(fic)):
+	fic[i] = fic[i].split(' ')
+	for v in fic[i]:
+		if not dico.has_key(v):
+			dico[v] = 1
+		else:
+			dico[v] += 1
 
-F=open('resultats.txt','w')
-for i in range(1,len(fic)) :
-	for k,v in dico.iteritems() :
-		if k :
+F = open('resultats.txt', 'w')
+for i in range(1,len(fic)):
+	for k, v in dico.iteritems():
+		if k:
 			F.write(str(k)+'\t'+str(v)+'\n')
-
 F.close()
 
 
-##### DNA : Counting DNA Nucleotides
-fic=read_fichier(fichier='rosalind_dna.txt')
-dico={'A':0 , 'T':0 , 'C':0 , 'G':0}
-for nt in fic[0] :
-	dico[nt]+=1
+# ##### DNA : Counting DNA Nucleotides
+fic = read_fichier(fichier='rosalind_dna.txt')
+dico = {'A': 0, 'T': 0, 'C' : 0, 'G': 0}
+for nt in fic[0]:
+	dico[nt] += 1
 
-F=open('resultats.txt','w')
-F.write(str(dico['A']) + str(dico['C']) + str(dico['G']) + str(dico['T']) )
+F = open('resultats.txt', 'w')
+F.write(str(dico['A']) + str(dico['C']) + str(dico['G']) + str(dico['T']))
 F.close()
 	
 
-##### RNA : Transcribing DNA into RNA 
-fic=read_fichier(fichier='rosalind_rna.txt')
-F=open('resultats.txt','w')
-for nt in fic[0] :
-	if nt=='T':	F.write('U')
-	else :	F.write(nt)
+# ##### RNA : Transcribing DNA into RNA
+fic = read_fichier(fichier='rosalind_rna.txt')
+F = open('resultats.txt','w')
+for nt in fic[0]:
+	if nt == 'T':
+		F.write('U')
+	else:
+		F.write(nt)
 
 F.close()
 
 
-##### REVC : Complementing a Strand of DNA
-fic=read_fichier(fichier='rosalind_revc.txt')
-dico={'A':'T' , 'T':'A' , 'C':'G' , 'G':'C'}
-l=len(fic[0])-1
-F=open('resultats.txt','w')
-for i in xrange(l+1) :
+# ##### REVC : Complementing a Strand of DNA
+fic = read_fichier(fichier='rosalind_revc.txt')
+dico = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+l = len(fic[0])-1
+F = open('resultats.txt', 'w')
+for i in xrange(l+1):
 	F.write(dico[fic[0][l-i]])
-
 F.close()
 
 
-##### Identifying Unknown DNA Quickly
+# ##### sset - Counting Subsets
+n = 833
+print(2**n % 1000000)
 
 
+# ##### lexf - Enumerating k-mers Lexicographically
+import itertools
 
+text = "A B C D E F".replace(" ", "")
+r = 3
+text="ATCG"; r=2
+result = set()
+for i in itertools.combinations_with_replacement(text, r):
+	for j in itertools.permutations(i, r):
+		result.add("".join(j))
+
+for i in sorted(result):
+	print(i)
 
 
 
